5f69ae8233948e11d5fd5742a1f893ea64e82725
Gottoz <got.ugu.pl@gmail.com>
2020-01-17 00:08:39 +0100

    added jobs in hompege

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Blog(models.Model):
+    publish_date = models.DateTimeField(default=timezone.now())
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
